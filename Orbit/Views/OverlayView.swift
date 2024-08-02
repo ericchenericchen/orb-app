7cf50140bf3633ad85dc83e10e107a44f464c20b
Eric Chen <ericsc@usc.edu>
2024-08-01 22:24:35 -0600

    RegisterView Progress 8/1

---
+++ Orbit/Views/OverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  OverlayView.swift
+//  Orbit
+//
+//  Created by Eric Chen on 7/25/24.
+//
+
+import SwiftUI
+
+struct OverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverlayView()
+}
